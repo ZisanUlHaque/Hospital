be54436bb5e76dad1a5bb6d3c63c7e4f82e557ba
Zisan <mdzisanulhaque989@gmail.com>
2025-01-23 00:02:03 +0600

    Doctor Details

--- app.js
+++ app.js
@@ -118,6 +118,29 @@ const handleSearch = () => {
     const value = document.getElementById("search").value;
     loadDoctors(value);
 };
+
+const loadReview = () => {
+  fetch("https://testing-8az5.onrender.com/doctor/review/")
+    .then((res) => res.json())
+    .then((data) => displayReview(data));
+};
+
+const displayReview = (reviews) => {
+  reviews.forEach((review) => {
+    const parent = document.getElementById("review-container");
+    const div = document.createElement("div");
+    div.classList.add("review-card");
+    div.innerHTML = `
+        <img src="./Images/pexels-markus-winkler-1430818-18512872.jpg" alt="" />
+            <h4>${review.reviewer}</h4>
+            <p>
+             ${review.body.slice(0, 100)}
+            </p>
+            <h6>${review.rating}</h6>
+        `;
+    parent.appendChild(div);
+  });
+};
 //   Calling Functions
 
 loadServices();
@@ -125,3 +148,4 @@ loadDoctors();
 loadDesignation();
 loadSpecialization();
 handleSearch();
+loadReview();
